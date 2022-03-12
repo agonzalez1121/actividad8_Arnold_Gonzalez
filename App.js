a7c63f1c79ef46044ac5d034749c1e310bc1bdde
Arnold Gonzalez V <a1121250001@utch.edu.mx>
2022-03-11 18:26:32 -0700

    Tarea 8 Unidad 2: Consumo de API en NodeJS

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
+import React, {useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , FlatList, ActivityIndicator} from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState([])
+  
+  const [data, setData] = useState([]);
+
+  const getPost = async() =>{
+  try {
+    const url ="https://jsonplaceholder.typicode.com/posts";
+  
+    //consumir datos
+
+    const response = await fetch(url);
+
+    //convertir a json
+
+    const json = await response.json();
+    setData(json);
+    
+    
+  } catch (error) {
+    console.error("error");
+  }
+
+     
+     
+    finally{
+      setLoading(false);
+    }
+    
+
+
+
+  }
+
+  useEffect(()=>{
+    getPost();
+  },[])
+  
+  
+  
+  
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    { isLoading ? <ActivityIndicator /> :(
+              <FlatList 
+                  data={data}
+                  keyExtractor = {({id}, index) => id}
+                  renderItem = {
+                    ({item}) =>(
+                      <Text>{item.title}</Text>
+                    )
+                    
+          
+                  }
+                   /> )
+    
+    }          
     </View>
   );
 }
